b1eb29e404855a99fdb154748e108ed2591b2437
MarionsPrivate <marionsprivate@gmail.com>
2014-03-20 23:43:42 +0100

    App zusammengefügt

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// We've added a third and final item to our tab panel - scroll down to see it
 Ext.application({
     name: 'Sencha',
 
@@ -7,6 +8,18 @@ Ext.application({
             tabBarPosition: 'bottom',
 
             items: [
+                {
+                    title: 'Home',
+                    iconCls: 'home',
+                    cls: 'home',
+                    html: [
+                        '<img width="65%" src="http://staging.sencha.com/img/sencha.png" />',
+                        '<h1>Welcome to Sencha Touch</h1>',
+                        "<p>We're creating the Getting Started app, which demonstrates how ",
+                        "to use tabs, lists, and forms to create a simple app.</p>",
+                        '<h2>Sencha Touch</h2>'
+                    ].join("")
+                },
                 {
                     xtype: 'nestedlist',
                     title: 'Blog',
@@ -33,7 +46,58 @@ Ext.application({
                                 rootProperty: 'responseData.feed.entries'
                             }
                         }
+                    },
+
+                    detailCard: {
+                        xtype: 'panel',
+                        scrollable: true,
+                        styleHtmlContent: true
+                    },
+
+                    listeners: {
+                        itemtap: function(nestedList, list, index, element, post) {
+                            this.getDetailCard().setHtml(post.get('content'));
+                        }
                     }
+                },
+                // this is the new item
+                {
+                    title: 'Contact',
+                    iconCls: 'user',
+                    xtype: 'formpanel',
+                    url: 'contact.php',
+                    layout: 'vbox',
+
+                    items: [
+                        {
+                            xtype: 'fieldset',
+                            title: 'Contact Us',
+                            instructions: '(email address is optional)',
+                            height: 285,
+                            items: [
+                                {
+                                    xtype: 'textfield',
+                                    label: 'Name'
+                                },
+                                {
+                                    xtype: 'emailfield',
+                                    label: 'Email'
+                                },
+                                {
+                                    xtype: 'textareafield',
+                                    label: 'Message'
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'button',
+                            text: 'Send',
+                            ui: 'confirm',
+                            handler: function() {
+                                this.up('formpanel').submit();
+                            }
+                        }
+                    ]
                 }
             ]
         });
